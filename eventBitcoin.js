14133199a4855b53a2d99b4ba1d639f553c12f8b
P.Getta <pgetta@plasticjam.com>
2014-12-08 16:16:50 +0200

    added Retina support for icon

--- eventBitcoin.js
+++ eventBitcoin.js
@@ -38,38 +38,78 @@ function updateBadge(start){
 				var icon = new Image();
 				var up_down_rate = new Image();
 				icon.src = "img/mp-icon.png";
-				if(course_BTC){up_down_rate.src = "img/up.png";}
-				else{up_down_rate.src = "img/down.png";}
 				icon.onload = function(){
+					if(course_BTC) {
+						up_down_rate.src = "img/up.png";
+					}
+					else {
+						up_down_rate.src = "img/down.png";
+					}
+
 					up_down_rate.onload = function(){
-				  		var canvas = document.createElement('canvas');
-						var c = canvas.getContext('2d');
-						c.beginPath();
-						c.fillStyle = '#fff';
-						c.moveTo(3, 1);
-						c.lineTo(17, 1); c.lineTo(19, 3); c.lineTo(19, 17); c.lineTo(17, 19); 
-						c.lineTo(3, 19); c.lineTo(1, 17); c.lineTo(1, 3); c.lineTo(3, 1);
-						c.fill();
-						c.drawImage(icon, 2, 1);
-						c.drawImage(up_down_rate, 11, 4);
-						c.beginPath();
-						c.fillStyle = '#000';
-						c.font = 'bold 9px sans-serif';
-						c.textBaseline = 'top';
-						var draw_price = ''+Math.round(curr_price_BTC);
-						var lng_draw_price = c.measureText(draw_price).width;
-						var left_margin = Math.floor((19 - lng_draw_price)/2);
-						c.fillText (draw_price, left_margin, 10);
-						c.fill();
-						var imageData = c.getImageData(0, 0, 19, 19);
-						chrome.browserAction.setIcon({
-							imageData: imageData
-						});
+
+						var isRetina = window.devicePixelRatio > 1;
+						var imageData = createIcon(curr_price_BTC, icon, up_down_rate, 1);
+
+						if(!isRetina){
+							chrome.browserAction.setIcon({
+								imageData: imageData
+							});
+						}
+						else {
+							var icon2X = new Image();
+							icon2X.src = "img/mp-icon@2x.png";
+							icon2X.onload = function() {
+								var up_down_rate2X = new Image();
+								if(course_BTC) {
+									up_down_rate2X.src = "img/up@2x.png";
+								}
+								else {
+									up_down_rate2X.src = "img/down@2x.png";
+								}
+								up_down_rate2X.onload = function() {
+									var imageData2X = createIcon(curr_price_BTC, icon2X, up_down_rate2X, 2);
+									chrome.browserAction.setIcon({
+										imageData: {
+											"19": imageData,
+											"38": imageData2X
+										}
+									});
+								};
+							};
+						}
+
 						chrome.storage.local.set({'price': curr_price_BTC});
+
 					};
+
 				};
 			}
       	});
     };
     req.send(null);
+}
+
+function createIcon(curr_price_BTC, icon, up_down_rate, scale) {
+	var canvas = document.createElement('canvas');
+	var c = canvas.getContext('2d');
+	c.beginPath();
+	c.fillStyle = '#fff';
+	c.moveTo(3*scale, 1*scale);
+	c.lineTo(17*scale, 1*scale); c.lineTo(19*scale, 3*scale); c.lineTo(19*scale, 17*scale); c.lineTo(17*scale, 19*scale);
+	c.lineTo(3*scale, 19*scale); c.lineTo(1*scale, 17*scale); c.lineTo(1*scale, 3*scale); c.lineTo(3*scale, 1*scale);
+	c.fill();
+	c.drawImage(icon, 2*scale, 1*scale);
+	c.drawImage(up_down_rate, 11*scale, 4*scale);
+	c.beginPath();
+	c.fillStyle = '#000';
+	c.font = scale>1? 'bold 18px sans-serif': 'bold 9px sans-serif';
+	c.textBaseline = 'top';
+	var draw_price = ''+Math.round(curr_price_BTC);
+	var lng_draw_price = c.measureText(draw_price).width;
+	var left_margin = Math.floor((19*scale - lng_draw_price)/2);
+	c.fillText (draw_price, left_margin, 10*scale);
+	c.fill();
+	var imageData = c.getImageData(0, 0, 19*scale, 19*scale);
+	return imageData;
 }
